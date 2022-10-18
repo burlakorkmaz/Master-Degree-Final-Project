e9f3b9baee5fe18d1e0a95c43ca4225d9fdec558
burlakorkmaz <burlanur1060@gmail.com>
2022-10-18 21:46:48 +0200

    train_test update

--- Train_test/test_cnn.py
+++ Train_test/test_cnn.py
@@ -3,15 +3,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from tensorflow.keras.models import load_model
 import cv2
-from tensorflow.keras.applications.vgg16 import preprocess_input
 from tensorflow.keras.utils import to_categorical
 #import scikitplot as skplt
 from sklearn.metrics import roc_curve, auc
 from sklearn.metrics import confusion_matrix
 import seaborn as sn
 
+
 img_size = 224
-batch_size = 32 
+batch_size = 32
 data_folder = "Dataset\\dolphin_signal_dataset\\Test\\"
 test_csv_path = 'Dataset\\dolphin_signal_dataset\\dolphin_signal_test.csv'
     
@@ -21,32 +21,31 @@ file_names = test_df['file_names'].values.tolist()
 labels = np.array(test_df['labels'].values.tolist(), dtype=np.float64)
 
 #%%
-model = load_model("models\\model_vgg.h5")
+model = load_model("models\\model_cnn.h5")
 
 #%%
 images_list = []
 counter = 1
 for file_name in file_names:
     path =  data_folder + file_name
-    image_array = cv2.imread(path)
+    image_array = cv2.imread(path, 0)
     image_array = cv2.resize(image_array, (img_size, img_size))
-    image_array = preprocess_input(image_array)
     images_list.append(image_array)
     if counter % 100 == 0:
         print(str(counter) + " image loaded...")
     counter += 1
-    
+
+images_list =  [x * (1./255) for x in images_list]    
 images_array = np.array(images_list, dtype=np.float32)
 
 predictions = model.predict(images_array, batch_size = batch_size, verbose = True)
-predictions = np.array(predictions)
 #%%
 
 test_labels = to_categorical(labels, num_classes=2)
 loss, acc = model.evaluate(images_array, test_labels, verbose = True)
 #%%    
 #ROC curves
-y_true = labels     
+y_true = labels    
 # skplt.metrics.plot_roc_curve(y_true, predictions)
 # plt.show()
 
@@ -59,7 +58,6 @@ plt.ylabel('True Positive Rate')
 plt.title('ROC')
 plt.legend(loc="lower right")
 plt.show()
-
 
 #%%
 #confusion matrix
--- Train_test/train_cnn.py
+++ Train_test/train_cnn.py
@@ -2,12 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import StratifiedKFold
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.applications.vgg16 import preprocess_input
-from tensorflow.keras.applications.vgg16 import VGG16
-from tensorflow.keras import layers, models
+from tensorflow.keras import models
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.models import load_model
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import Conv2D, Dense, Flatten, MaxPooling2D, Dropout
 from sklearn.metrics import roc_curve, auc
 from scipy import interp
 import numpy as np
@@ -15,41 +14,71 @@ import gc
 
 #%% NECESSARY VARIABLES
 
-IMG_SIZE = 224
+#IMG_SIZE = 224
+
+width = 224   #400
+height = 224  #300
+
 batch_size = 32
 directory = "Dataset\\dolphin_signal_dataset\\Train"
 
 train_data = pd.read_csv('Dataset\\dolphin_signal_dataset\\dolphin_signal_train.csv')
 train_data = train_data.sample(frac=1, random_state=2).reset_index() # shuffle data
 
-
 train_data = train_data.astype({"labels" : str})
 
 labels = train_data['labels']
 
-skf = StratifiedKFold(n_splits = 5, random_state = 7, shuffle = True)
+skf = StratifiedKFold(n_splits = 5, random_state = 7, shuffle = True) 
 
-idg = ImageDataGenerator(preprocessing_function = preprocess_input) 
+
+idg = ImageDataGenerator(rescale = 1./255) 
 
 #%% MODEL
 
-def model(IMG_SIZE):
-    ## Loading VGG16 model
-    base_model = VGG16(weights="imagenet", include_top=False, input_shape=(IMG_SIZE,IMG_SIZE,3))
-    base_model.trainable = True 
-    
-    model = models.Sequential([
-    base_model,
-    layers.Flatten(),
-    layers.Dense(50, activation='relu'),
-    layers.Dense(20, activation='relu'),
-    layers.Dense(2, activation='softmax')])
+def model(width,height):
+    model = models.Sequential()
+
+    model.add(
+        Conv2D(
+            filters = 16,
+            kernel_size = (7,7),
+            strides = 2,
+            activation = "linear",  
+            padding='same',
+            input_shape = (height,width,1)
+        )
+    )
+
+    model.add(MaxPooling2D(pool_size = 2, strides=None))
+
+    model.add(Dropout(0.2))
+     
+    model.add(
+         Conv2D(
+             filters = 32,
+             kernel_size = (5,5),
+             strides = 2,
+             activation = "linear",   
+             padding='same'
+         )
+     )
+     
+    model.add(MaxPooling2D(pool_size = 2, strides=None))
+
+    model.add(Flatten())
+
+    model.add(Dense(32, activation='relu')),
+    model.add(Dense(16, activation='relu')),
+    model.add(Dense(2, activation='softmax'))
     return model
 
 #%%
+
 fpr_plot_list = []
 tpr_plot_list = []
 label_plot_list = []
+
 
 first_run = True
 
@@ -63,11 +92,11 @@ mean_fpr = np.linspace(0,1,100)
 counter = 1
 # enumerate splits
 for train, val in skf.split(train_data, labels):
-    print('train: %s, test: %s' % (train_data.iloc[train], train_data.iloc[val]))
+    #print('train: %s, test: %s' % (train_data.iloc[train], train_data.iloc[val]))
     
     training_data = train_data.iloc[train]
     validation_data = train_data.iloc[val]
-
+    
     del train, val
     gc.collect()
     
@@ -80,8 +109,8 @@ for train, val in skf.split(train_data, labels):
                                                    shuffle = False,
                                                    class_mode='categorical',
                                                    classes = ["0","1"],
-                                                   color_mode="rgb",
-                                                   target_size = (IMG_SIZE, IMG_SIZE))
+                                                   color_mode="grayscale",
+                                                   target_size = (height, width))
 
     valid_data_generator  = idg.flow_from_dataframe(dataframe = validation_data,
                                                    directory = directory,
@@ -92,24 +121,26 @@ for train, val in skf.split(train_data, labels):
                                                    shuffle = False,
                                                    class_mode='categorical',
                                                    classes = ["0","1"],
-                                                   color_mode="rgb",
-                                                   target_size = (IMG_SIZE, IMG_SIZE))
+                                                   color_mode="grayscale",
+                                                   target_size = (height, width))
     
    
     if first_run:
-        model = model(IMG_SIZE)
+        model = model(width, height)
         first_run = False
         
     else:
-        model = load_model("models\\model_vgg.h5")
+        model = load_model("models\\model_cnn.h5") 
     
     
-    optimizer = Adam(learning_rate = 0.00001, beta_1=0.9, beta_2=0.999,epsilon=1e-7,amsgrad=False,name='Adam')    
+    optimizer = Adam(lr = 0.0001) # 0.0001
     model.compile(
-    optimizer=optimizer,
-    loss='binary_crossentropy',
-    metrics=['accuracy'])
-
+        optimizer = optimizer,
+        loss = 'binary_crossentropy',   
+        metrics = ['accuracy'],
+    )
+    
+    
     es = EarlyStopping(monitor='val_loss', mode='min', patience=15,  restore_best_weights=True)
     
     history = model.fit(
@@ -118,6 +149,8 @@ for train, val in skf.split(train_data, labels):
         validation_data=valid_data_generator,
         batch_size=batch_size, 
         callbacks=[es])  
+    
+    
     
     # summarize history for loss
     plt.plot(model.history.history['loss'])
@@ -148,7 +181,6 @@ for train, val in skf.split(train_data, labels):
     list_loss.append(loss)
     list_acc.append(acc)
     
-    
     # for avg roc
     predictions = model.predict_generator(valid_data_generator, steps = len(valid_data_generator.labels))
     predictions = np.array(predictions)
@@ -162,11 +194,12 @@ for train, val in skf.split(train_data, labels):
     tpr_plot_list.append(tpr)
     label_plot_list.append(str('ROC fold %d (AUC = %0.2f)' % (counter, roc_auc)))
           
-    model.save("models\\model_vgg.h5")
+    model.save("models\\model_cnn.h5") 
     
     counter+=1
     del training_data, validation_data
     gc.collect()
+  
 
 #%%  
 
@@ -175,6 +208,7 @@ avg_acc = sum(list_acc) / len(list_acc)
 print("avg loss:", str(avg_loss), "avg acc:", avg_acc)
 
 #%%
+
 for i in range(len(tpr_plot_list)):
     plt.plot(fpr_plot_list[i], tpr_plot_list[i], lw=2, alpha=0.3, label=label_plot_list[i])
 
@@ -190,8 +224,9 @@ plt.ylabel('True Positive Rate')
 plt.title('ROC')
 plt.legend(loc="lower right")
 
-title = "figs\\avg_roc_vgg.png"
+title = "figs\\avg_roc_cnn.png"
 plt.savefig(title)
 plt.show()
 
 #%%
+
